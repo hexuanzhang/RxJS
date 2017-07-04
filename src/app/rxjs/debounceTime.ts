d1e6dc7c8aafddde1afeee2448c920afe26b457a
hexuanzhang <136789658@qq.com>
2017-07-04 22:26:52 +0800

    updade package.json

--- src/app/rxjs/debounceTime.ts
+++ src/app/rxjs/debounceTime.ts
@@ -3,14 +3,27 @@ import { Observable } from 'rxjs';
 
 @Component ({
 	selector: 'dobounceTime',
-	template: '<h3>dobounceTime</h3>'
+	template: '<input type="text" id="text">' +
+	'<div>input: <span>{{ value }}</span></div>'
 })
 export class DobounceTimeComponent implements OnInit {
 	constructor () {}
 	
+	value: string;
+	
+	/**
+	 * 用于控制发送频率的操作符，与 delay 相似，延迟指定的时间再发送值，如果期间有新的值则只发送新的值
+	 */
 	ngOnInit () {
-		// Observable.interval(1500).dobounceTime(2000).subscribe(x => {
-		// 	console.info(x);
-		// });
+		Observable.fromEvent(document.getElementById('text'), 'input')
+			.debounceTime(500)
+			.pluck('target', 'value')
+			.map(v => String(v).trim())
+			.filter(v => v !== '')
+			.distinctUntilChanged()
+			.subscribe(v => {
+				console.info(v);
+				this.value = String(v);
+			});
 	}
 }
