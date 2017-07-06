3a55c08b9ffb484a51d4d3aec820862160dbf5a4
hexuanzhang <136789658@qq.com>
2017-07-06 23:09:25 +0800

    update

--- src/app/component/encapsulation.ts
+++ src/app/component/encapsulation.ts
@@ -16,6 +16,8 @@ export class EncapsulationComponent {
 	
 	/**
 	 * 枚举类 ViewEncapsulation 包括三个成员：Native、Emulated、None
-	 *      Native: 
+	 *      Native: 使用原生 Shadow Dom 特性
+	 *      Emulated: 默认设置，使用 Angular 提供的样式包装机制来封装组件，使组件的样式不影响其他组件
+	 *      None：使用原生样式，不使用 Shadow Dom 和样式包装机制
 	 */
 }
